19e560942f3e3c06242e8c76dd1ff240c5a085c7
Joel <jcalifa@gmail.com>
2011-05-09 05:23:17 -0400

    Calendar Dots and To-Do Working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,8 @@
+class Event < Struct.new(:class, :name, :date); end
+
 class StudentsController < ApplicationController
+
+  helper_method :human_date
 
   require 'table_builder'
   require 'table_builder/calendar_helper'
@@ -17,19 +21,45 @@ class StudentsController < ApplicationController
         @tasks << Event.new(course.name, assignment.title, assignment.due_date)
       end
     end
+    @tasks.sort! {|x,y| x.date <=> y.date }
+    @tasks = @tasks.slice!(0,7)
   end
 
-#  <% tasks.each do |hw| %>
-#      <% hw.each do |h,w| %>
-#        <%= w %> ++++
-#      <%end %>     <br/>
-#  <%end %>
-#
-#
-#   <% homework.each do |hw| %>
-#      <%= hw['title'] %><br />
-#  endclass Event < Struct.new(:id, :name, :date); end
+  def human_date(date)
+    string = ""
+    level = 3
+    now = Time.now.to_date
+
+    if date >= now.end_of_week.next_week
+      string = date.strftime("%m/%d").gsub('0','')
+    end
+
+    if date < now.end_of_week.next_week
+      string = "Next #{date.strftime('%A')}"
+    end
+
+    if date < now.end_of_week
+      string = "This #{date.strftime('%A')}"
+    end
+
+    if date < now + 5.days
+      level = 2
+    end
+
+    if date < now + 2.days
+      level = 1
+    end
+
+    if date == now + 1.days
+      string = "Tomorrow!"
+    end
+
+    if date < now
+      string = "Overdue!"
+    end
+
+    [string, level]
+  end
+
+
 end
-
-
-class Event < Struct.new(:class, :name, :date); end
